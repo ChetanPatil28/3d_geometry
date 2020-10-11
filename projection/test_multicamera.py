b76e36b1a23b72156eef1e5775f63e718705756f
ChetanPatil28 <pchetan481@gmail.com>
2020-10-11 20:48:30 +0530

    Switching between different co-ordinate systems

--- projection/test_multicamera.py
+++ projection/test_multicamera.py
@@ -4,16 +4,11 @@ import utils
 from utils import Camera
 
 np.random.seed(79)
-# np.set_printoptions(precision=4)
 np.set_printoptions(precision=3)
 
 pts3d_11 = np.random.randint(11, 50, size=(8, 3))  # 8-points
 K = np.load("../camMatrix_720p.npy")
 dist = np.zeros(shape=5)
-
-
-
-
 
 # This is the first camera with same orientation and same place as origin.
 Cam11 = Camera(K)
@@ -45,23 +40,14 @@ R13_est1, R13_est2, t13_est = cv2.decomposeEssentialMat(Ess13)
 Ess23, _ = cv2.findEssentialMat(pts2d_12, pts2d_13, K, cv2.FM_RANSAC)
 R23_est1, R23_est2, t23_est = cv2.decomposeEssentialMat(Ess23)
 
-print("R estimated is equal ???  ", (Cam12_R - R12_est1).sum(), (Cam12_R- R12_est2).sum())
+print("R estimated is equal ???  ", (Cam12_R - R12_est1).sum(), (Cam12_R - R12_est2).sum())
 print("Is the t equal ??? ", (utils.norm(Cam12_t) - t12_est).sum())
 
-
-
 # Q1. What is the location of the `Points` as seen by the Cam3 ??
-# Ans. P' = [R | -R*C] * P
-pts3d_3 = np.matmul(Cam11.Rt, utils.euc_to_hom(pts3d_11).T) # [3, N]
-
-# Q2. How to verify if the above formula is correct ?.
-# Ans. Well, reverse-transform.
-pts3d_11_est = np.matmul(np.hstack((Cam13.R.T, -Cam13.R.T.dot(Cam13.t))), utils.euc_to_hom(pts3d_3.T).T)
-
-print(pts3d_11_est.T -  pts3d_11)
-# print((utils.hom_to_euc(pts3d_3est) - pts3d_3).sum())
-
-print("cam3 shape  ", pts3d_11_est.shape)
-
-
-# print(np.hstack((Cam13.R.T, -Cam13.R.T.dot(Cam13.t))))
+# Ans. P' = [R | t] * P
+pts3d_13 = np.matmul(Cam13.Rt, utils.euc_to_hom(pts3d_11).T)  # [3, N]
+# How to verify if the above formula is correct ?.
+# Well, reverse-transform to get the original points ie P = [R.T | -R.T*t] * P'
+pts3d_11_est = np.matmul(np.hstack((Cam13.R.T, -Cam13.R.T.dot(Cam13.t))), utils.euc_to_hom(pts3d_13.T).T).T
+# Let's see if the estimated and the original are same.
+print("Are both same ", (pts3d_11 - pts3d_11_est).sum())
